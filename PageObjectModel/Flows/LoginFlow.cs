7788e1047012f6f8a7bb6cf66751cb22c15a68f3
vip0720 <vish7.pujari@gmail.com>
2024-09-05 16:15:26 +0530

    Add project files.

---
+++ PageObjectModel/Flows/LoginFlow.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Playwright;
+using PageObjectModel.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel.Flows
+{
+    public class LoginFlow
+    {
+        public LoginPage page;
+        public LoginFlow(LoginPage _page)
+        {
+            page = _page;
+        }
+
+        private string url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+        private string username = "Admin";
+        private string password = "admin123";
+        
+        public async void NavigateToApplication()
+        {
+            await page.page.GotoAsync(url);
+        }
+        public async Task LoginWithValidUserAsync()
+        {
+            page.UserName.FillAsync(username);
+            page.Password.FillAsync(password);
+            await page.LoginButton.ClickAsync();
+        }
+    }
+}
---
+++ PageObjectModel/Pages/LoginPage.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel.Pages
+{
+
+    public class LoginPage
+    {
+        public IPage page;
+        public LoginPage(IPage _page)
+        {
+            page = _page;
+        }
+        public ILocator UserName
+        {
+            get
+            {
+                return page.GetByText("Username");
+            }
+        }
+
+        public ILocator Password
+        {
+            get
+            {
+                return page.GetByText("Password");
+            }
+        }
+        public ILocator LoginButton
+        {
+            get
+            {
+                return page.GetByLabel("Login");
+            }
+        }
+
+    }
+}
